35c8d5308e70782ae74f7436f9a78bc07a9e9dff
bokeron8 <franco123rex777@gmail.com>
2024-04-24 14:38:10 -0300

    Added navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,35 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  import { Carousel } from 'flowbite-svelte';
+  import suspicaciasLogo from "./assets/suspicacias.ico"
+  import { AddressBookOutline } from 'flowbite-svelte-icons';
+  import {images} from './images.js'
+  
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main class="">
+  <Navbar fluid="true">
+    <NavBrand href="/">
+      <img src={suspicaciasLogo} class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold text-red-600">Suspicacias</span>
+    </NavBrand>
+    <NavHamburger menuClass="dark:text-red-600"/>
+    <NavUl>
+      <NavLi href="/">Inicio</NavLi>
+      <NavLi href="/productos">Productos</NavLi>
+      <NavLi href="/contacto">Contacto</NavLi>
+    </NavUl>
+  </Navbar>
+  <div class="h-100" imgClass="object-contain h-full w-fit rounded-sm">
+    <Carousel {images} let:Controls>
+      <Controls />
+    </Carousel>
+  </div>  
 </main>
 
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
---
+++ src/images.js
@@ -0,0 +1,8 @@
+
+  export const images = [
+    {
+      alt: 'Cosmic timetraveler',
+      src: 'https://unsplash.com/photos/KdZvvyemkPM/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjg5ODk2OTM3fA&force=true&w=1920',
+      title: 'cosmic-timetraveler-pYyOZ8q7AII-unsplash.com'
+    },
+  ];
