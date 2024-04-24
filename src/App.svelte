1dfc4117f83bc8abcaa9ea193c72867fbfc33d1a
Bokeron8 <67697305+Bokeron8@users.noreply.github.com>
2024-04-24 18:47:23 +0000

    Deploying to gh-pages from @ Bokeron8/suspicasias-svelte@e6feb20bebd947ce1dffbd01d29e49042fff7044 🚀

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  import { Carousel } from 'flowbite-svelte';
+  import suspicaciasLogo from "./assets/suspicacias.ico"
+  import { AddressBookOutline } from 'flowbite-svelte-icons';
+  import {images} from './images.js'
+  
+</script>
+
+
+
+
+<main class="">
+  <Navbar fluid="true">
+    <NavBrand href="/">
+      <img src={suspicaciasLogo} class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold text-red-600">Suspicacias</span>
+    </NavBrand>
+    <NavHamburger menuClass="dark:text-red-600"/>
+    <NavUl>
+      <NavLi href="/">Inicio</NavLi>
+      <NavLi href="/productos">Productos</NavLi>
+      <NavLi href="/contacto">Contacto</NavLi>
+    </NavUl>
+  </Navbar>
+  <div class="">
+    <Carousel class="sm:h-screen xl:h-18" {images} let:Controls imgClass="h-full w-fit rounded-sm">
+      <Controls />
+    </Carousel>
+  </div>  
+  <h1>texto por aca</h1>
+</main>
+
+
+<style>
+</style>
---
+++ src/images.js
@@ -0,0 +1,20 @@
+import image1 from './assets/1.webp'
+import image2 from './assets/2.webp'
+import image3 from './assets/3.webp'
+  export const images = [
+    {
+      alt: 'Cosmic timetraveler',
+      src: image1,
+      title: 'cosmic-timetraveler-pYyOZ8q7AII-unsplash.com'
+    },
+    {
+      alt: 'Cosmic timetraveler',
+      src: image2,
+      title: 'cosmic-timetraveler-pYyOZ8q7AII-unsplash.com'
+    },
+    {
+      alt: 'Cosmic timetraveler',
+      src: image3,
+      title: 'cosmic-timetraveler-pYyOZ8q7AII-unsplash.com'
+    },
+  ];
